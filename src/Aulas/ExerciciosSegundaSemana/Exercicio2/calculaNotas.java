b5dcaa3550a8a5d8f55e5cdca90d66e91c5c77f6
EdersonDeMelo <drsnmelo@gmail.com>
2022-04-07 18:47:53 -0300

    Implementado metodo para calcular a situacao aprovado/recuperacao do aluno

--- src/Aulas/ExerciciosSegundaSemana/Exercicio2/calculaNotas.java
+++ src/Aulas/ExerciciosSegundaSemana/Exercicio2/calculaNotas.java
@@ -2,37 +2,45 @@ package Aulas.ExerciciosSegundaSemana.Exercicio2;
 
 public class calculaNotas {
 
-	public static void main(String[] args) {
-		criarAluno();
-	}
-	
-	public static void criarAluno() {
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setId(1);
-		disciplina1.setDisciplina("POO");
+    public static void main(String[] args) {
+        criarAluno();
+    }
 
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Ederson");
-		aluno1.setDisciplina(disciplina1);
-		aluno1.getDisciplina().setNotaPrimeiraProva(8.8);
-		aluno1.getDisciplina().setNotaSegundaProva(1.9);
-		aluno1.getDisciplina().setNotaTrabalho(7);
+    public static void criarAluno() {
 
-		double media = 0;
-		media = notaPonderada(aluno1.getDisciplina().getNotaPrimeiraProva(), 
-				aluno1.getDisciplina().getNotaSegundaProva(), 
-				aluno1.getDisciplina().getNotaTrabalho());
-		
-	System.out.println((media>=7)? "Aprovado" : "Recupera��o");
-		
-	}
+        Disciplina disciplina1 = new Disciplina();
+        disciplina1.setId(1);
+        disciplina1.setDisciplina("POO");
 
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Ederson");
+        aluno1.setMatricula(1546);
+        aluno1.setDisciplina(disciplina1);
+        aluno1.getDisciplina().setNotaPrimeiraProva(8.8);
+        aluno1.getDisciplina().setNotaSegundaProva(7.9);
+        aluno1.getDisciplina().setNotaTrabalho(7);
 
-	public static double notaPonderada(double nota1, double nota2, double trabalho) {
-		double media;
-		media = ((nota1*1.5) +(nota2*1.5)+(trabalho*2)) /5;
-		return media;
-		
-	}
+        double media = 0;
+        media = mediaPonderada(aluno1.getDisciplina().getNotaPrimeiraProva(),
+                aluno1.getDisciplina().getNotaSegundaProva(),
+                aluno1.getDisciplina().getNotaTrabalho());
+        situacao(media);
 
+        System.out.println("Aluno: " + aluno1.getNome() + "\nMatricula: " + aluno1.getMatricula() +
+                "\nMateria: " + aluno1.getDisciplina().getDisciplina() + "\nNota da Primeira Prova: "
+                + aluno1.getDisciplina().getNotaPrimeiraProva() +
+                "\nNota da Segunda Prova: " + +aluno1.getDisciplina().getNotaSegundaProva() + "\n" +
+                "Nota do Trabalho: " + aluno1.getDisciplina().getNotaTrabalho() + "\n" +
+                "Resultado Final:" + situacao(media));
+    }
+
+    public static double mediaPonderada(double nota1, double nota2, double trabalho) {
+        double media;
+        media = ((nota1 * 1.5) + (nota2 * 1.5) + (trabalho * 2)) / 5;
+        return media;
+    }
+
+    public static String situacao(double media) {
+        return ((media >= 7) ? "Aprovado" : "Recuperação");
+    }
 }
